f3f9ae33b9ecd58e975f3d6ffff14bfcd66854e0
moriyaohana <moriyaohana@gmail.com>
2026-05-29 11:50:57 +0300

    fix: Fixed broken migration

--- src/migrations/1700000012000-AddSoftDeleteToCascadeChildren.ts
+++ src/migrations/1700000012000-AddSoftDeleteToCascadeChildren.ts
@@ -13,15 +13,9 @@ export class AddSoftDeleteToCascadeChildren1700000012000 implements MigrationInt
     await queryRunner.query(
       `ALTER TABLE "ticket_dependencies" ADD COLUMN "deletedAt" TIMESTAMP WITH TIME ZONE NULL`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "tickets" ADD COLUMN "deletedByCascade" BOOLEAN NOT NULL DEFAULT false`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "tickets" DROP COLUMN "deletedByCascade"`,
-    );
     await queryRunner.query(
       `ALTER TABLE "ticket_dependencies" DROP COLUMN "deletedAt"`,
     );
